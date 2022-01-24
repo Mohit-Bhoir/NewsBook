bde78be6f398fff5454d9d3786f136cc9a104806
Mohit Bhoir <96228063+Mohit-Bhoir@users.noreply.github.com>
2022-01-24 17:22:05 +0530

    Main nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,32 +8,32 @@ export class Navbar extends Component {
     return <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <Link className="navbar-brand" to="/">NewsBook</Link>
+    <Link className="navbar-brand" to="/NewsBook/">NewsBook</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+          <Link className="nav-link active" aria-current="page" to="/NewsBook/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/Business">Business</Link>
+          <Link className="nav-link" to="/NewsBook/Business">Business</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/Health">Health</Link>
+          <Link className="nav-link" to="/NewsBook/Health">Health</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/Entertainment">Entertainment</Link>
+          <Link className="nav-link" to="/NewsBook/Entertainment">Entertainment</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/Science">Science</Link>
+          <Link className="nav-link" to="/NewsBook/Science">Science</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/Sports">Sports</Link>
+          <Link className="nav-link" to="/NewsBook/Sports">Sports</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/Technology">Technology</Link>
+          <Link className="nav-link" to="/NewsBook/Technology">Technology</Link>
         </li>
       </ul>
     </div>
